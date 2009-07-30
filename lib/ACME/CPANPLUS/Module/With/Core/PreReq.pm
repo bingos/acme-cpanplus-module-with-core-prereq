e6700ff662469ed75c67e001640a48e7c6d94faf
Chris Williams <chris@bingosnet.co.uk>
2009-07-30 14:59:43 +0100

    general dist making

--- lib/ACME/CPANPLUS/Module/With/Core/PreReq.pm
+++ lib/ACME/CPANPLUS/Module/With/Core/PreReq.pm
@@ -8,3 +8,32 @@ our $VERSION = '0.02';
 qq[Nobody here but us chickens];
 
 __END__
+
+=head1 NAME
+
+ACME::CPANPLUS::Module::With::Core::PreReq - Fake module with a prereq that is a core module for testing CPANPLUS
+
+=head1 SYNOPSIS
+
+ # erm
+
+ cpanp -i ACME::CPANPLUS::Module::With::Core::PreReq
+
+=head1 DESCRIPTION
+
+ACME::CPANPLUS::Module::With::Core::PreReq is a fake module that has a prerequisite of a core module
+so I can test something in L<CPANPLUS> and L<CPANPLUS::YACSmoke>
+
+No moving parts and nothing to see.
+
+=head1 AUTHOR
+
+Chris C<BinGOs> Williams <chris@bingosnet.co.uk>
+
+=head1 LICENSE
+
+Copyright E<copy> Chris Williams
+
+This module may be used, modified, and distributed under the same terms as Perl itself. Please see the license that came with your Perl distribution for details.
+
+=cut
