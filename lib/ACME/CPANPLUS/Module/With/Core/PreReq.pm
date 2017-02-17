260c99ad38a01d364fa8f325171781de8f061610
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2017-02-17 17:02:27 +0000

    Switched to Dist::Zilla

--- lib/ACME/CPANPLUS/Module/With/Core/PreReq.pm
+++ lib/ACME/CPANPLUS/Module/With/Core/PreReq.pm
@@ -1,17 +1,13 @@
 package ACME::CPANPLUS::Module::With::Core::PreReq;
 
+#ABSTRACT: Fake module with a prereq that is a core module for testing CPANPLUS
+
 use strict;
 use warnings;
 
-our $VERSION = '0.04';
-
 qq[Nobody here but us chickens];
 
-__END__
-
-=head1 NAME
-
-ACME::CPANPLUS::Module::With::Core::PreReq - Fake module with a prereq that is a core module for testing CPANPLUS
+=pod
 
 =head1 SYNOPSIS
 
@@ -25,15 +21,5 @@ ACME::CPANPLUS::Module::With::Core::PreReq is a fake module that has a prerequis
 so I can test something in L<CPANPLUS> and L<CPANPLUS::YACSmoke>
 
 No moving parts and nothing to see.
-
-=head1 AUTHOR
-
-Chris C<BinGOs> Williams <chris@bingosnet.co.uk>
-
-=head1 LICENSE
-
-Copyright E<copy> Chris Williams
-
-This module may be used, modified, and distributed under the same terms as Perl itself. Please see the license that came with your Perl distribution for details.
 
 =cut
